2f87810dbc9b596e847793d3963866580da28770
Ben Wiley <ben.wiley@newspring.cc>
2015-01-25 12:27:03 -0500

    Add git url

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'bjwiley2:server-watch',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Enables third party server pinging functionality',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/NewSpring/meteor-serverWatch.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
